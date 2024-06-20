81fc4d50393943d964ad568ff694047f8ebcc62d
merchant <saboormerchant6@gmail.com>
2024-06-20 22:31:12 +0500

    Removed some code

--- bootstrap.php
+++ bootstrap.php
@@ -6,17 +6,5 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 use Merchant\NuclearOrm\Core\Nuclear;
 
-$nuclear = new Nuclear([
-    'connection' => 'mysql',
-    'host' => '127.0.0.1',
-    'database' => 'test',
-    'user' => 'merchant',
-    'password' => 'Thealien862!',
-    'port' => 3306,
-    'persistent' => true,
-]);
 
-$nuclear->boot();
-
-$test = new \Merchant\NuclearOrm\Models\Test();
 
